229d5bfc6a09a0892c8ea4c52f59fe06c57e3504
Paul Ivan Dela Cruz <kmpkhai001@gmail.com>
2023-08-27 00:01:21 +0800

    first commit

---
+++ app/components/TodoList.tsx
@@ -0,0 +1,31 @@
+import { ITask } from "@/types/tasks"
+
+interface TodoListProps {
+  tasks: ITask[];
+}
+
+const TodoList: React.FC<TodoListProps> = ({ tasks }) => {
+  return (
+    <div className="overflow-x-auto">
+        <table className="table">
+            {/* head */}
+            <thead>
+            <tr>
+                <th>Tasks</th>
+                <th>Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+              {tasks.map((task) => (
+                <tr key={task.id}>
+                  <td>{task.text}</td>
+                  <td>Blue</td>
+                </tr>
+              ))}
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default TodoList
